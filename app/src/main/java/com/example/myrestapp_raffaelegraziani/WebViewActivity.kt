a085c58598b1d511083f4def69806416c75b4453
raffaeleGraz <raffaele.graziani06@gmail.com>
2025-05-13 12:41:54 +0200

    Effettuato collegamento al sito Netflix

--- app/src/main/java/com/example/myrestapp_raffaelegraziani/WebViewActivity.kt
+++ app/src/main/java/com/example/myrestapp_raffaelegraziani/WebViewActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myrestapp_raffaelegraziani
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,9 @@ class WebViewActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_web_view)
 
+        val webView = findViewById<WebView>(R.id.WebViewNetflix)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.netflix.com/it/")
     }
 }
